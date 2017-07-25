6af8c611f976dd5181d5aeabdfbd753ece3ad3d2
Peter Kaplan <pkap@umich.edu>
2017-07-25 15:11:53 -0700

    working auth for google

--- parse.js
+++ parse.js
@@ -24,38 +24,12 @@ function sherlocker(text, eventData) {
 
 function getLocation(text){
 	//goog stuff
+	const Language = require('@google-cloud/language');
+	const projectId = 'pic2cal';
+	const language = Language({
+ 		projectId: projectId,
+    	keyFilename: './keys.json'
+	});
 
-
-// Google cloud settings
-// Run npm install --save @google-cloud/language
-
-const Language = require('@google-cloud/language');
-
-// Your Google Cloud Platform project ID
-const projectId = 'pic2cal';
-
-// Instantiates a client
-const language = Language({
-  projectId: projectId
-});
-
-	//var key = 'AIzaSyD0qmIEyByT-RzF-mu4arUZOutGaozpjEk';
-
-	//var url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=' + key;
 	
-// The text to analyze
-const text2 = 'Hello, world!';
-
-// Detects the sentiment of the text
-language.detectSentiment(text2)
-  .then((results) => {
-    const sentiment = results[0];
-
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
 }

7ace8e7e4772dabbb61d498e7636d997c34f530e
Peter Kaplan <pkap@umich.edu>
2017-07-25 14:33:53 -0700

    Google Cloud settings

---
+++ locationParser.js
@@ -0,0 +1,6 @@
+
+// Google cloud settings
+// Run npm install --save @google-cloud/language
+var key = 'AIzaSyD0qmIEyByT-RzF-mu4arUZOutGaozpjEk';
+var url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=' + key;
+
--- parse.js
+++ parse.js
@@ -24,4 +24,24 @@ function sherlocker(text, eventData) {
 
 function getLocation(text){
 	//goog stuff
+
+// Google cloud settings
+// Run npm install --save @google-cloud/language
+var key = 'AIzaSyD0qmIEyByT-RzF-mu4arUZOutGaozpjEk';
+var url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=' + key;
+
+
+	var optionspost = {
+	    host : 'api.projectoxford.ai',
+	    path : '/vision/v1.0/ocr?language=en&detectOrientation=true',
+	    method : 'POST',
+	};
+
+	// do the POST call
+	var reqPost = https.request(optionspost, function(res1) {
+
+	    res1.on('data', function(OCRResponse) {
+	        imageToText(OCRResponse, res);
+	    });
+	});
 }

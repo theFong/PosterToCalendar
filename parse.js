89553c898d8256876e819df98cfb9247f6f7b777
Peter Kaplan <pkap@umich.edu>
2017-07-25 14:34:27 -0700

    Google Cloud settings: g

--- parse.js
+++ parse.js
@@ -27,7 +27,6 @@ function getLocation(text){
 
 // Google cloud settings
 // Run npm install --save @google-cloud/language
-var key = 'AIzaSyD0qmIEyByT-RzF-mu4arUZOutGaozpjEk';
 var url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=' + key;
 
 
